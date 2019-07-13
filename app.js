635ecd9837cef4250517557fb3d77c5306fef676
gbatistuti <gustavo.batistuti1@gmail.com>
2019-07-13 17:15:57 -0300

    Correçao de erros no app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Controll-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accpet, Authorization');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
         return res.status(200).json({});
@@ -37,5 +37,6 @@ app.use((error, req, res, next) => {
         }
     })
 });
+
 
 module.exports = app;
